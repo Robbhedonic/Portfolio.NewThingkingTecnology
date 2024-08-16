86231cc54b428657845f4cd41258ce0f9fc097f0
Robbhedonic <rccstockholm@outlook.com>
2024-08-16 14:39:09 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,205 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+if(navToggle){
+    navToggle.addEventListener('click', ()=> {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    //when we click on each nav__link, we remove the show-menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'), 
+skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills(){
+    let itemClass = this.parentNode.className
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    } if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
+
+
+/*==================== SERVICES MODAL ====================*/
+
+const modalViews = document.querySelectorAll('.services__modal'),
+modalBtns = document.querySelectorAll('.services__button'),
+modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+modalBtns.forEach((modalBtn,i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+modalCloses.forEach((modalClose, i) => {
+    modalClose.addEventListener('click', () =>{
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id')
+
+        // Concatenar el selector de manera segura
+        const link = document.querySelector('.nav__menu a[href*="' + sectionId + '"]');
+
+        // Verificar si el link existe
+        if (link) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                link.classList.add('active-link')
+            } else {
+                link.classList.remove('active-link')
+            }
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
+ 
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+  // Add or remove the dark theme / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+});
+
+/*==================== Contact email ====================*/ 
+
+
+// Inicializa EmailJS con tu usuario ID
+(function () {
+  emailjs.init("ozTt0iYMM0Ckh7h92");
+})();
+
+// Maneja el evento de envío del formulario
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Validar el campo de nombre para asegurarse de que contiene al menos dos palabras
+    const fullName = document.getElementById("fullName").value.trim();
+    const nameParts = fullName.split(' ');
+
+    if (nameParts.length < 2) {
+      alert("Please enter both your first name and last name.");
+      return; // Detiene el envío del formulario
+    }
+
+    let params = {
+      from_name: fullName,
+      email_id: document.getElementById("email_id").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_y1topoy", "template_p6ijg4x", params).then(function (res) {
+      alert("Success! Your message has been sent.");
+      
+      // Limpia los campos del formulario
+      document.getElementById("contactForm").reset();
+    }).catch(function (error) {
+      alert("Failed to send your message. Please try again.");
+    });
+  });
+});
